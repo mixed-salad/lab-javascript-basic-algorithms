b582aaceefda22c7f2dcdde49da281094b1d1738
Harumi Terayama <hterayama@gmail.com>
2020-10-24 16:35:20 +0200

    modified iteration 3.3

--- js/index.js
+++ js/index.js
@@ -36,9 +36,9 @@ for(let i = hacker2.length-1; i >= 0; i--) {
 console.log(name2);
 
 //3.3
-if (hacker1[0].localeCompare(hacker2)) {
+if (hacker1.localeCompare(hacker2)) {
   console.log(`The driver's name goes first.`)
-} else if (hacker2[0].localeCompare(hacker1)) {
+} else if (hacker2.localeCompare(hacker1)) {
   console.log(`Yo, the navigator goes first definitely.`)
 } else {
   console.log(`What?! You both have the same name?`)
